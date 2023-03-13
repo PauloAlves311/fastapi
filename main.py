9dfe306b5d535dadaf256a6e9d2088cdc3628e6c
PauloAlves311 <40123939+PauloAlves311@users.noreply.github.com>
2023-03-13 21:50:05 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import json
 from urllib.parse import urlparse
 # from typing import Union
 # from fastapi.responses import PlainTextResponse
 # from pydantic import BaseModel
 
-import download
-download.downloadData()
+#import download
+#download.downloadData()
 
 app = FastAPI()
 
@@ -14,7 +14,12 @@ load = json.load(open("data.json"))["imoveis"]["imovel"]
 
 
 @app.get("/")
-async def read_index():
+async def read_index(
+    zona: str = None,
+    min_price: int = None,
+    max_price: int = None,
+    obj: str = None,
+    ):
     houses = []
     destaque = 0
     for house in load:
@@ -24,20 +29,38 @@ async def read_index():
             elif type(imagem) is dict:
                 if imagem["principal"] == "1":
                     url = "https://cdn1.ximocrm.com/i/704ED61D-335D-46AF-813E-00DD2E2BB6F4_" + house["id"] + "_0______" + urlparse(imagem["url"]).path.rsplit('/', 1)[-1]
+        
+        if "Land" in house["tipo"]["#text"] or "Plot" in house["tipo"]["#text"]:
+            titulo = house["tipo"]["#text"]
+        else:
+            titulo = house["tipo"]["#text"] + ", T" + house["nquartos"]
+
+        if zona and house["localidade"] != zona:
+            continue
+
+        if obj and house["objectivo"]["#text"] != obj:
+            continue
+
+        if (min_price and int(house["precoweb"]) < min_price) or (max_price and int(house["precoweb"]) > max_price):
+            continue
 
         houses.append({
             "id": house["id"],
             "sort": house["ref"],
-            "titulo": house["titulo"],
+            "titulo": titulo,
+            "zona": house["localidade"],
             "preco": house["precoweb"],
-            "url": url,
+            "objectivo": house["objectivo"]["#text"],
+            "imagem": url,
              })
 
         if house["destaque"] == "1":
             destaque += 1
     print(destaque)
+    print(len(houses))
 
     return houses
+
             
 
 
@@ -54,7 +77,7 @@ async def find_house(house_id: str):
                 "objectivo": house["objectivo"]["#text"] if house["objectivo"]["@id"] != "0" else "N/A",
                 "distrito": house["distrito"]["#text"] if house["distrito"]["@id"] != "0" else "N/A",
                 "concelho": house["concelho"]["#text"] if house["concelho"]["@id"] != "0" else "N/A",
-                "freguesia": house["freguesia"]["#text"] if house["freguesia"]["@id"] != "0" else "",
+                "freguesia": house["freguesia"]["#text"] if house["freguesia"]["@id"] != "0" else "N/A",
                 "zona": house["zona"],
                 "localidade": house["localidade"],
                 "cp": house["cp"],
