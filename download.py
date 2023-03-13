ef92bc18c7698ca18e41e49962761942498c3a21
PauloAlves311 <40123939+PauloAlves311@users.noreply.github.com>
2023-03-13 17:06:25 +0000

    Add files via upload

---
+++ download.py
@@ -0,0 +1,25 @@
+import xmltodict
+import xml.etree.ElementTree as ET
+
+import requests
+import json
+
+URL = "http://services.ximo.pt/export_xml_base/default.aspx?h=704ED61D-335D-46AF-813E-00DD2E2BB6F4&p=MFWLFBRPVSAXGBHWCZQLMBSDFBUJY-422377&l=en"
+
+response = requests.get(URL)
+content = response.content
+
+with open('feed.xml', 'wb') as file:
+    file.write(response.content)
+
+root = ET.parse("feed.xml").getroot()
+
+xmlstring = ET.tostring(root)
+xmldict = xmltodict.parse(xmlstring)
+
+with open('data.json', 'w') as f:
+    json.dump(xmldict, f)
+
+
+# file = open('data.json')
+# data = json.load(file)
